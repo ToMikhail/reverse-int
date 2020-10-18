f6158e40c20e63e06ab482f0c6b6767bc2a56d2b
To Mikha <toustsikmikhail@gmail.com>
2020-10-18 16:45:30 +0300

    feat: add function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let num = '';
+    let arr = String(n).split('');
+
+    if (arr[0] === '-' && arr[arr.length - 1] === '0') {
+        for (let i = arr.length - 2; i > 0; i--) {
+            num += arr[i];
+        }
+    } else if (arr[0] === '-') {
+        for (let i = arr.length - 1; i > 0; i--) {
+            num += arr[i];
+        }
+    } else if (arr[arr.length - 1] === '0') {
+        for (let i = arr.length - 2; i >= 0; i--) {
+            num += arr[i];
+        }
+    } else {
+        for (let i = arr.length - 1; i >= 0; i--) {
+            num += arr[i];
+        }
+    }
+    reversed = +num;
+    return reversed;
 }
+
